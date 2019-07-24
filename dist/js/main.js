c5c1327f1bc0b81006d0aa269bf6e040b74dfe81
bbehrang <behvandi.behrang@gmail.com>
2019-07-24 14:17:04 +0300

    Recommendations applied

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,23 +1,25 @@
+
+function toggleMenu(menu, bars, opened){
+    console.log("here");
+    console.log(menu);
+    $(menu).css('display', 'block');
+    $(bars).eq(1).css('opacity', (opened == 0 ? '0' : '1'));
+
+    $(bars).eq(0).css({'transform': `rotate(${(opened == 0) ? '45deg' : '0'}) 
+                                    translate(${(opened == 0) ? '3px, 10px' : '0,0'})`});
+    $(bars).eq(0).css({'width': opened == 0 ? '27px' : '21px'});
+    $(bars).eq(2).css({'transform':`rotate(${(opened == 0) ? '-45deg' : '0'}) 
+                                    translate(${(opened == 0) ? '2px, -10px' : '0,0'})`});
+    $(menu).animate({opacity: opened == 0 ? 1 : 0}, {queue: false, duration:300, complete : function(){
+            if(opened == 1){
+                $(menu).css('display', 'none');
+            }
+        }});
+    return (opened == 0 ? 1 : 0);
+}
 $(document).ready(function(e){
     $('.hamburger-menu').on('click', function(e){
-        if($(this).attr('data-clicked') == 0){
-            $('.mobile-menu').css('display', 'block');
-            $('.hamburger-menu__bars').eq(1).css('opacity', '0');
-            $('.hamburger-menu__bars').eq(0).css({'transform': 'rotate(45deg) translate(3px, 10px)','width': '27px'});
-            $('.hamburger-menu__bars').eq(2).css({'transform': 'rotate(-45deg) translate(2px, -10px)'});
-            $('.mobile-menu').animate({opacity: 1}, {queue: false, duration:300});
-            $(this).attr('data-clicked' , 1);
-        }
-        else{
-            $(this).attr('data-clicked' , 0);
-            $('.hamburger-menu__bars').eq(1).css('opacity', '1');
-            $('.hamburger-menu__bars').eq(0).css({'transform': 'rotate(0) translate(0, 0)', 'width' : '21px'});
-            $('.hamburger-menu__bars').eq(2).css({'transform': 'rotate(0) translate(0, 0 )'});
-            $('.mobile-menu').animate({opacity: 0}, {queue: false, duration:300, complete: function(){
-                    $('.mobile-menu').css('display', 'none');
-                }});
-
-        }
-
+        e.preventDefault();
+        $(this).attr('data-clicked', toggleMenu('.navbar-menu', '.hamburger-menu__bars', $(this).attr('data-clicked')));
     });
 });
